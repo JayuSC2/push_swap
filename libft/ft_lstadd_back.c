2974d6439e04e41cd511e78d2192bc2283946105
Jayu <jayuze@gmail.com>
2023-12-22 11:31:36 +0100

    added all

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 17:27:55 by juitz             #+#    #+#             */
+/*   Updated: 2023/12/22 11:20:42 by julian           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *last_element;
+	last_element = ft_lstlast(*lst);
+	last_element->next = new;
+}
+/* int	main(void)
+{
+	t_list *first_element;
+	
+} */
